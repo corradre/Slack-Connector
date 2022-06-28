7f4e77fb0981a7ac0d735b8027f0700a8f732043
corradre <corradre@mulesoft.com>
2022-06-28 11:38:36 -0300

    second commit

--- src/main/java/org/mule/extension/internal/SlackEndPoints.java
+++ src/main/java/org/mule/extension/internal/SlackEndPoints.java
@@ -1,7 +1,6 @@
 package org.mule.extension.internal;
 
 public class SlackEndPoints {
-
     public static final String BASE_URI = "https://slack.com/api";
     public static final String AUTH_TEST = "/auth.test";
     public static final String CONVERSATION_LIST = "/conversations.list";
